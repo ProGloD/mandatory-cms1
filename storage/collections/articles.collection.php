e3eec986cafc99967b8caa088b9a2d80f381dc75
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-09-04 13:23:30 +0200

    added storage

---
+++ storage/collections/articles.collection.php
@@ -0,0 +1,116 @@
+<?php
+ return array (
+  'name' => 'articles',
+  'label' => '',
+  '_id' => 'articles5d669e35920b0',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'body',
+      'label' => '',
+      'type' => 'markdown',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'published_on',
+      'label' => '',
+      'type' => 'date',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'author',
+      'label' => '',
+      'type' => 'collectionlink',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+        'link' => 'authors',
+        'multiple' => false,
+        'display' => 'name',
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => false,
+  'in_menu' => false,
+  '_created' => 1567006261,
+  '_modified' => 1567447244,
+  'color' => '',
+  'acl' => 
+  array (
+    'public' => 
+    array (
+      'entries_view' => true,
+    ),
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+      'enabled' => false,
+    ),
+    'read' => 
+    array (
+      'enabled' => false,
+    ),
+    'update' => 
+    array (
+      'enabled' => false,
+    ),
+    'delete' => 
+    array (
+      'enabled' => false,
+    ),
+  ),
+  'icon' => 'post.svg',
+);
